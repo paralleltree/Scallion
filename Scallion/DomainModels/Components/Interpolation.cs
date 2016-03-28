a45a1b8627cebd243079b08cec23b8b6c30c6789
Paralleltree <paralleltree@outlook.com>
2016-03-28 23:04:01 +0900

    Add component classes

---
+++ Scallion/DomainModels/Components/Interpolation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scallion.DomainModels.Components
+{
+    public class Interpolation
+    {
+        public InterpolationParameter First { get; set; }
+        public InterpolationParameter Second { get; set; }
+
+        public Interpolation()
+        {
+            First = new InterpolationParameter(20, 20);
+            Second = new InterpolationParameter(107, 107);
+        }
+    }
+
+    public struct InterpolationParameter
+    {
+        public byte X { get; set; }
+        public byte Y { get; set; }
+
+        public InterpolationParameter(byte x, byte y)
+            : this()
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ Scallion/Raw/Components/BoneInterpolation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Scallion.Core;
+using Scallion.DomainModels.Components;
+
+namespace Scallion.Raw.Components
+{
+    internal abstract class BoneInterpolation : MMDObject
+    {
+        public Interpolation X { get; set; }
+        public Interpolation Y { get; set; }
+        public Interpolation Z { get; set; }
+        public Interpolation R { get; set; }
+
+        public BoneInterpolation()
+        {
+            X = new Interpolation();
+            Y = new Interpolation();
+            Z = new Interpolation();
+            R = new Interpolation();
+        }
+
+
+        public static implicit operator DomainModels.Components.BoneInterpolation(BoneInterpolation b)
+        {
+            return new DomainModels.Components.BoneInterpolation()
+            {
+                X = b.X,
+                Y = b.Y,
+                Z = b.Z,
+                R = b.R
+            };
+        }
+    }
+}
