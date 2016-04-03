1611f1804d008dee43befc8f4d38268bdbb03041
Paralleltree <paralleltree@outlook.com>
2016-04-03 21:34:27 +0900

    Add public motion test

---
+++ Scallion.Tests/UnitTests/DomainModels/MotionTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+using Scallion.DomainModels;
+
+namespace Scallion.Tests.UnitTests.DomainModels
+{
+    internal class MotionTest : IOTest<Motion>
+    {
+        protected override IEnumerable<object[]> TestCases()
+        {
+            return Directory.EnumerateFiles(@"Resources\Binaries", "*.vmd")
+                .Select(p =>
+                {
+                    string name = Path.GetFileNameWithoutExtension(p);
+                    return new object[]
+                    {
+                        string.Format(@"Resources\Binaries\{0}.vmd",name),
+                        string.Format(@"Resources\Fixtures\DomainModels\{0}.json",name)
+                    };
+                });
+        }
+    }
+}
