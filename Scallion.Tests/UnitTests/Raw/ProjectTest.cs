cb8ea005b4a928c25096e0bb082055e06f292b61
Paralleltree <paralleltree@outlook.com>
2016-05-15 22:21:47 +0900

    Add raw project test

---
+++ Scallion.Tests/UnitTests/Raw/ProjectTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+using Scallion.Raw;
+
+namespace Scallion.Tests.UnitTests.Raw
+{
+    internal class ProjectTest : IOTest<Project>
+    {
+        protected override IEnumerable<object[]> TestCases()
+        {
+            return Directory.EnumerateFiles(@"Resources\Binaries", "*.pmm")
+                .Select(p =>
+                {
+                    string name = Path.GetFileNameWithoutExtension(p);
+                    return new object[]
+                    {
+                        string.Format(@"Resources\Binaries\{0}.pmm", name),
+                        string.Format(@"Resources\Fixtures\Raw\{0}.json", name)
+                    };
+                });
+        }
+    }
+}
