40973d3cebf3bd1d96abb1a154a255c29534c2f2
Paralleltree <paralleltree@outlook.com>
2016-09-10 22:50:01 +0900

    Add raw pose test

---
+++ Scallion.Tests/UnitTests/Raw/PoseTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+using Scallion.Raw;
+
+namespace Scallion.Tests.UnitTests.Raw
+{
+    internal class PoseTest : IOTest<Pose>
+    {
+        protected override IEnumerable<object[]> TestCases()
+        {
+            return Directory.EnumerateFiles(@"Resources\Binaries", "*.vpd")
+                .Select(p =>
+                {
+                    string name = Path.GetFileNameWithoutExtension(p);
+                    return new object[]
+                    {
+                        string.Format(@"Resources\Binaries\{0}.vpd", name),
+                        string.Format(@"Resources\Fixtures\Raw\{0}.json", name)
+                    };
+                });
+        }
+    }
+}
