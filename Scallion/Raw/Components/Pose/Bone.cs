cb6b73776e410a3867ca1084716e33545215a64c
Paralleltree <paralleltree@outlook.com>
2016-09-10 22:46:01 +0900

    Implement raw pose class

---
+++ Scallion/Raw/Components/Pose/Bone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Numerics;
+
+namespace Scallion.Raw.Components.Pose
+{
+    internal class Bone
+    {
+        public string Name { get; set; }
+        public Vector3 Position { get; set; }
+        public Quaternion Quaternion { get; set; }
+    }
+}
